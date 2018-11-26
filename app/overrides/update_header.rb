0b0bd931896f6c4a59ebf67c8f4471f62745626b
Matheus Andrade <matheus.andrade.santos@gmail.com>
2018-11-26 15:53:07 -0200

    Updated override, added logos

--- app/overrides/update_header.rb
+++ app/overrides/update_header.rb
@@ -1,7 +1,8 @@
-#=begin
 Deface::Override.new(:virtual_path => 'spree/shared/_header',
 	:name => 'change store logo',
 	:replace => 'figure#logo',
-	:text => "<%= link_to image_tag('logos/logo_small.png') %>"
-	)
-#=end
+	:text => "<figure id='logo' class='col-md-4 col-sm-3' data-hook>
+          		<%= link_to image_tag('logos/logo.png') %>
+        	</figure>"
+	#:text => "<<%= link_to image_tag('logos/logo_small.png') %> "
+	) 
