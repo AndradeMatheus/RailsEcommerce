1eb77e5ffc61777466848831445fe7376907b398
Matheus Andrade <matheus.andrade.santos@gmail.com>
2018-11-26 14:57:24 -0200

    Updated override logo, cleaned workspace

--- app/overrides/update_header.rb
+++ app/overrides/update_header.rb
@@ -2,6 +2,6 @@
 Deface::Override.new(:virtual_path => 'spree/shared/_header',
 	:name => 'change store logo',
 	:replace => 'figure#logo',
-	:text => "<%= image_tag('/images/logo.png') %>"
+	:text => "<%= link_to image_tag('logos/logo_small.png') %>"
 	)
 #=end
